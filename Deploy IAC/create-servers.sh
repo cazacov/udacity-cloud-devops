93f2d23695fd81f9edea1fd99bbd86ecebc1decb
Victor Cazacov <cazacov.git@gmail.com>
2020-06-27 22:49:08 +0200

    Add IAM-role and instance profile

--- Deploy IAC/create-servers.sh
+++ Deploy IAC/create-servers.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-aws cloudformation create-stack --stack-name udacity-devops-iac-servers --template-body file://servers.yml --parameters file://params-servers.json --region us-west-2
+aws cloudformation create-stack --stack-name udacity-devops-iac-servers --template-body file://servers.yml --parameters file://params-servers.json --region us-west-2  --capabilities CAPABILITY_NAMED_IAM
--- Deploy IAC/update-servers.sh
+++ Deploy IAC/update-servers.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-aws cloudformation update-stack --stack-name udacity-devops-iac-servers --template-body file://servers.yml --parameters file://params-servers.json --region us-west-2
+aws cloudformation update-stack --stack-name udacity-devops-iac-servers --template-body file://servers.yml --parameters file://params-servers.json --region us-west-2  --capabilities CAPABILITY_NAMED_IAM
