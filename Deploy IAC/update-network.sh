7e2e5f0b7e5a39ce03c5c049250d8cce1d7025e5
Victor Cazacov <cazacov.git@gmail.com>
2020-06-28 21:20:08 +0200

    Output load-balancer URL

--- Deploy IAC/update-network.sh
+++ Deploy IAC/update-network.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-aws cloudformation update-stack --stack-name udacity-devops-iac --template-body file://network.yml --parameters file://params-network.json --region us-west-2
+aws cloudformation update-stack --stack-name udacity-devops-iac-network --template-body file://network.yml --parameters file://params-network.json --region us-west-2
